74f78a5a92038e68712315efd4b3b4254704bb28
AmanSrivastava12 <70564443+AmanSrivastava12@users.noreply.github.com>
2022-05-29 22:34:23 +0530

    Updated the JS file

--- app.js
+++ app.js
@@ -24,3 +24,34 @@ var updateClock = function () {
 updateClock();
 var oneSecond = 1000;
 setInterval(updateClock, oneSecond);
+
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+const feelslikeElement = document.querySelector("#feels-like p");
+const tempminElement = document.querySelector("#temp-min p");
+const tempmaxElement = document.querySelector("#temp-max p");
+const sunriseElement = document.querySelector("#sunrise p");
+const pressureElement = document.querySelector("#pressure p");
+const humidityElement = document.querySelector("#humidity p");
+const windspeedElement = document.querySelector("#wind-speed p");
+const windgustElement = document.querySelector("#wind-gust p");
+const sunsetElement = document.querySelector("#sunset p");
+
+const weather = {};
+
+weather.temperature = {
+  unit: "celsius",
+};
+
+const KELVIN = 273;
+const key = "c827a72464a888d8dfd3dbea735dcbde";
+
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+  notificationElement.style.display = "block";
+  notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
